dc5fca6bc21e8f40319ee72066af9e4218d42708
mahinagar863-ctrl <mahinagar863@gmail.com>
2026-06-11 15:37:48 +0530

    create solution for repeated substring pattern problem

---
+++ WEEK_2/DAY_3/repeating_substring_pattern.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+
+        int n = s.length();
+
+        for (int len = 1; len <= n / 2; len++) {
+
+            if (n % len != 0) {
+                continue;
+            }
+
+            string pattern = s.substr(0, len);
+            string temp = "";
+
+            int repeat = n / len;
+
+            for (int i = 0; i < repeat; i++) {
+                temp += pattern;
+            }
+
+            if (temp == s) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
