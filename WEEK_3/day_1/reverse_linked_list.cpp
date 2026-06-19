c8299e79802e301ad1998b3097cca8b4fc72bec0
mahinagar863-ctrl <mahinagar863@gmail.com>
2026-06-19 17:03:06 +0530

    add reverse linked list function

---
+++ WEEK_3/day_1/reverse_linked_list.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr){
+            ListNode* nxt = curr->next;
+            curr->next = prev;
+
+            prev = curr;
+            curr = nxt;
+        }
+
+        return prev;
+    }
+};
