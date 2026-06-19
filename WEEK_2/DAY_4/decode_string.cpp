fc388797dba3543bbb2f6ac1343ffa6f5cfbcd81
mahinagar863-ctrl <mahinagar863@gmail.com>
2026-06-19 17:28:53 +0530

    add decodeString function for clarity

--- WEEK_2/DAY_4/decode_string.cpp
+++ WEEK_2/DAY_4/decode_string.cpp
@@ -1,49 +1,35 @@
 class Solution {
 public:
     string decodeString(string s) {
-
         stack<int> countStack;
         stack<string> stringStack;
-
         string currString = "";
         int num = 0;
-
         for (char ch : s) {
-
             if (isdigit(ch)) {
                 num = num * 10 + (ch - '0');
             }
-
             else if (ch == '[') {
                 countStack.push(num);
                 stringStack.push(currString);
-
                 num = 0;
                 currString = "";
             }
-
             else if (ch == ']') {
-
                 int repeat = countStack.top();
                 countStack.pop();
-
                 string prevString = stringStack.top();
                 stringStack.pop();
-
                 string temp = "";
-
                 for (int i = 0; i < repeat; i++) {
                     temp += currString;
                 }
-
                 currString = prevString + temp;
             }
-
             else {
                 currString += ch;
             }
         }
-
         return currString;
     }
 };
