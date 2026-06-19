0292f507f6aa9dae65a233f157d766d12cb2444d
mahinagar863-ctrl <mahinagar863@gmail.com>
2026-06-19 17:29:32 +0530

    add generate_parentheses.cpp for cleaner code
    Removed unnecessary whitespace and cleaned up formatting.

--- WEEK_2/DAY_4/generate_parentheses.cpp
+++ WEEK_2/DAY_4/generate_parentheses.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-    
     void solve(int open, int close, string curr, vector<string>& ans, int n) {
-
         if (curr.length() == 2 * n) {
             ans.push_back(curr);
             return;
@@ -14,13 +12,9 @@ public:
             solve(open, close + 1, curr + ')', ans, n);
         }
     }
-
     vector<string> generateParenthesis(int n) {
-
         vector<string> ans;
-
         solve(0, 0, "", ans, n);
-
         return ans;
     }
 };
